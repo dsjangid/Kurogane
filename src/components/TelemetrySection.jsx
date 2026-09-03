8e6d8bf86af58a7addeb8f7d442cb0613b626e28
dsjangid <dsj654@gmail.com>
2026-09-03 18:57:11 +0530

    style(components): add 2x2 impact stat counters in Telemetry

--- src/components/TelemetrySection.jsx
+++ src/components/TelemetrySection.jsx
@@ -3,3 +3,5 @@ import React from 'react';
 export default function TelemetrySection() {
   return <section id="telemetry" className="py-20">TELEMETRY SECTION</section>;
 }
+
+// Adding 2x2 stat counter matrix
