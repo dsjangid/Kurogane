6373981d6e1c8d69098a33338e627bad89520e8b
dsjangid <dsj654@gmail.com>
2026-09-03 18:57:09 +0530

    feat(components): scaffold LoadingScreen component

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,5 @@
+
+import React from 'react';
+export default function LoadingScreen() {
+  return <div className="fixed inset-0 bg-black z-50 flex items-center justify-center text-white">INITIALIZING...</div>;
+}
