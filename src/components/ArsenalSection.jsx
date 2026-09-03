a5655a6bfa442294f3472ec3fae6f52b272e828f
dsjangid <dsj654@gmail.com>
2026-09-03 18:57:11 +0530

    feat(components): implement weapon category filtering tabs in Arsenal

--- src/components/ArsenalSection.jsx
+++ src/components/ArsenalSection.jsx
@@ -3,3 +3,5 @@ import React from 'react';
 export default function ArsenalSection() {
   return <section id="arsenal" className="py-20">ARSENAL SECTION</section>;
 }
+
+// Adding category filter tabs
