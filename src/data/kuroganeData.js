9941fbd8fbc0845a63025d374eb2494b9ce3b341
dsjangid <dsj654@gmail.com>
2026-09-03 18:57:08 +0530

    feat(data): define weapon loadouts and specifications

--- src/data/kuroganeData.js
+++ src/data/kuroganeData.js
@@ -1,2 +1,6 @@
 // Kurogane Data Definitions
 export const siteMeta = { title: 'KUROGANE' };
+
+export const arsenalData = [
+  { id: 'blade-01', name: 'Muramasa Carbon Katana', class: 'Melee', dps: 1840 }
+];
