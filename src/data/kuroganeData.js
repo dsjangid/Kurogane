267c42f7a5484ad2fc3a6b68bec6d731e64a34f9
dsjangid <dsj654@gmail.com>
2026-09-03 18:57:08 +0530

    feat(data): define chassis mecha telemetry schemas

--- src/data/kuroganeData.js
+++ src/data/kuroganeData.js
@@ -4,3 +4,7 @@ export const siteMeta = { title: 'KUROGANE' };
 export const arsenalData = [
   { id: 'blade-01', name: 'Muramasa Carbon Katana', class: 'Melee', dps: 1840 }
 ];
+
+export const chassisData = [
+  { id: 'chassis-ironmask', name: 'TYPE-09 IRONMASK', armor: 'TITANIUM-CARBIDE' }
+];
